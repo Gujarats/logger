7aace6808c0fae420ebe0c9bb028868bed28af6a
gujarats <gujarat.santana@gmail.com>
2017-11-15 06:39:32 +0700

    debug style message

---
+++ debug.go
@@ -0,0 +1,24 @@
+package logger
+
+type DebugStyle struct {
+	PrefixColor  Attribute
+	MessageColor Attribute
+	Style        Attribute
+}
+
+func (d *DebugStyle) getDebugColorFormat(prefix, message string) string {
+	message = GetColorFormat(d.MessageColor, d.Style, message)
+	prefix = GetColorFormat(d.PrefixColor, d.Style, prefix)
+
+	return prefix + message
+}
+
+func NewDebugStyle(prefix, message string) string {
+	debug := DebugStyle{
+		PrefixColor:  Magenta,
+		MessageColor: Yellow,
+		Style:        Faint,
+	}
+
+	return debug.getDebugColorFormat(prefix, message)
+}
