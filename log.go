14e5ac73a0b66cc0df5064bc84fd526f2dfb0269
gujarats <gujarat.santana@gmail.com>
2017-11-15 06:40:23 +0700

    custom writter for io.Writter

---
+++ log.go
@@ -0,0 +1,12 @@
+package logger
+
+import "fmt"
+
+// creating empty struct and the method for satisfaying io.Writer interface
+type customWriter struct{}
+
+// this is for customize the output format time
+func (writer *customWriter) Write(bytes []byte) (int, error) {
+	timeFormatted := NewTimeStyle() + string(bytes)
+	return fmt.Print(timeFormatted)
+}
