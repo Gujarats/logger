1c2f083c135e2cca166153c5ccc9be69748035a0
gujarats <gujarat.santana@gmail.com>
2017-11-18 05:12:56 +0700

    create separate Debug function for only print

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"log"
 )
 
-func Debug(prefix, message string) {
+// Print Debug message and save that message into a file = logger.log
+func Debugf(prefix, message string) {
 	//Create debug message with default style
 	debugStyle := NewDebugStyle(prefix, message)
 	timeStyle := NewTimeStyle()
@@ -15,4 +16,15 @@ func Debug(prefix, message string) {
 	log.Println(debugStyle)
 
 	saveDebugMessage(timeStyle, debugStyle)
+}
+
+// Print debug message to os.StdOut without save it to a file
+func Debug(prefix, message string) {
+	//Create debug message with default style
+	debugStyle := NewDebugStyle(prefix, message)
+
+	// setup log
+	log.SetFlags(0)
+	log.SetOutput(new(customWriter))
+	log.Println(debugStyle)
 }
